e593f5ace18086ca3b55654897d1b825bffcb8a8
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2024-09-18 08:50:10 -0700

    Add personal customizations

--- users/miryoku-steve/custom_config.h
+++ users/miryoku-steve/custom_config.h
@@ -5,3 +5,22 @@
 
 #pragma once
 
+
+#define MIRYOKU_LAYER_BASE \
+KC_Q,              KC_W,              KC_F,              KC_P,              KC_G,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
+LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_D,              KC_H,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),      \
+LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_K,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
+U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
+
+// For gaming:
+#define MIRYOKU_LAYER_TAP \
+KC_Q,              KC_W,              KC_F,              KC_P,              KC_G,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
+KC_A,              KC_R,              KC_S,              KC_T,              KC_D,              KC_H,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),      \
+KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_K,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_LSFT,           LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
+#define MIRYOKU_ALTERNATIVES_TAP_QWERTY_MAYBE \
+KC_T,              KC_Q,              KC_W,              KC_E,              KC_R,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+KC_G,              KC_A,              KC_S,              KC_D,              KC_F,              KC_H,              LSFT_TKC_J,              KC_K,              KC_L,              KC_QUOT,           \
+KC_B,              KC_Z,              KC_X,              KC_C,              KC_V,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            LT(U_SYM,KC_ENT),  KC_BSPC,           KC_DEL,            U_NP,              U_NP
+
